f1ff7efd7548c022b5fa96dbe534332d5e570011
noviantisafitri <121856489+noviantisafitri@users.noreply.github.com>
2023-06-30 10:02:55 +0800

    Update About.js

--- src/section/About.js
+++ src/section/About.js
@@ -1,27 +1,35 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import img1 from '../assets/logo.png'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { about } from "../card/About";
+import { Image } from "react-bootstrap";
+import img1 from "../assets/logo.png";
 
 function About() {
   return (
-    <Container>
+    <div
+      className="d-flex align-items-center justify-content-center"
+      style={{ width: "100vw", height: "100vh" }}
+    >
+      <Container>
         <Row>
-            <div className='header-about'>INSEVENT</div>
+          <div className="header-about fs-1">INSEVENT</div>
         </Row>
         <Row>
-        <Col style={{textAlign:'justify'}} xs={6}>
+          <Col style={{textAlign:'justify'}} xs={6}>
             <h2>About This Event</h2>
-            <p className='text-about'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis. Nibh scelerisque ac adipiscing velit non nulla in amet pellentesque. Sit turpis pretium eget maecenas. Vestibulum dolor mattis consectetur eget commodo vitae.
-            </p>
-        </Col>
-        <Col>
-            <img src={img1} className='img-about'></img>
-        </Col>
+            <p className="text-about fs-6">{about.text}</p>
+            <p className="text-about fs-6">{about.text2}</p>
+          </Col>
+          <Col>
+            <Image
+              src={img1}
+              className="img-fluid align-items-center justify-content-center"
+              style={{ width: "50vw", height: "50vh" }}
+            />
+          </Col>
         </Row>
-    </Container>
+      </Container>
+    </div>
   );
 }
-
-export default About;
