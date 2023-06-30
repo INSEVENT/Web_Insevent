d1ee76b1a591f7ae7914b69e81f821c3818251ee
noviantisafitri <121856489+noviantisafitri@users.noreply.github.com>
2023-06-30 10:09:40 +0800

    Add files via upload

---
+++ src/data/about.js
@@ -0,0 +1,13 @@
+export const deskripsi_event = {
+  text: [
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa sollicitudin amet augue. 
+    Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis. Nibh scelerisque ac adipiscing velit non nulla in amet pellentesque. 
+    Sit turpis pretium eget maecenas. Vestibulum dolor mattis consectetur eget commodo vitae.`,
+  ],
+  text2: [
+    `  Amet pellentesque sit pulvinar lorem mi a, euismod risus rhoncus. Elementum ullamcorper nec, habitasse vulputate. 
+    Eget dictum quis est sed egestas tellus, a lectus. Quam ullamcorper in fringilla arcu aliquet fames arcu.Lacinia eget faucibus urna, nam risus nec elementum cras porta. 
+    Sed elementum, sed dolor purus dolor dui. Ut dictum nulla pulvinar vulputate sit sagittis in eleifend dignissim. Natoque mauris cras molestie velit. 
+    Maecenas eget adipiscing quisque viverra lectus arcu, tincidunt ultrices pellentesque.`,
+  ],
+};
