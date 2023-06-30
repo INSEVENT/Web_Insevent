b394cb2ef6c0812a48ade7b238a949aceceded40
noviantisafitri <121856489+noviantisafitri@users.noreply.github.com>
2023-06-30 11:19:58 +0800

    Add files via upload

---
+++ src/data/card.js
@@ -0,0 +1,34 @@
+import img1 from "../assets/logo.png";
+
+const cardData = [
+  {
+    imgSrc: img1,
+    category: "Category 1",
+    title: "Title 1",
+    description: "Description 1",
+    readMore: "Read More"
+  },
+  {
+    imgSrc: img1,
+    category: "Category 2",
+    title: "Title 2",
+    description: "Description 2",
+    readMore: "Read More"
+  },
+  {
+    imgSrc: img1,
+    category: "Category 3",
+    title: "Title 3",
+    description: "Description 3",
+    readMore: "Read More"
+  },
+  {
+    imgSrc: img1,
+    category: "Category 4",
+    title: "Title 4",
+    description: "Description 4",
+    readMore: "Read More"
+  },
+];
+
+export default cardData;
