7233101b2156558353ce5802ed36b7febcab6baa
noviantisafitri <121856489+noviantisafitri@users.noreply.github.com>
2023-07-15 11:54:59 +0800

    Update Comp.js

---
+++ src/pages/Comp.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Competition() {
+
+  return (
+    <div>
+      <h1 className="text-dark">Competition</h1>
+    </div>
+  );
+}
+
+export default Competition;
