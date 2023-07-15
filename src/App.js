7c49d8306348530dc0a0dcb36e7652ea987177b3
noviantisafitri <121856489+noviantisafitri@users.noreply.github.com>
2023-07-15 11:53:50 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import Navi from './component/Navi.js';
-import Carou from './section/Carou';
-import About from './section/About';
-import Blog from './section/Blog';
-import Foot from './component/Foot';
-import FaQ from './section/FaQ';
-import React, { useEffect, useState, useRef } from 'react';
-import { Link, animateScroll as scroll } from 'react-scroll';
-import Talkshow from './section/Talkshow';
-import Sponsor from './section/Sponsor';
-import { Container, Image } from 'react-bootstrap';
+import "./App.css";
+import Navi from "./component/Navi.js";
+import Carou from "./section/Carou";
+import About from "./section/About";
+import Blog from "./section/Blog";
+import Foot from "./component/Foot";
+import FaQ from "./section/FaQ";
+import React, { useEffect, useState, useRef } from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
+import Talkshow from "./section/Talkshow";
+import Sponsor from "./section/Sponsor";
+import { Container, Image } from "react-bootstrap";
 import img1 from "./assets/logo.png";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import Competition from "./pages/Comp";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -28,16 +30,20 @@ function App() {
       setTimeout(() => {
         scroll.scrollTo(targetRef.current.offsetTop, {
           duration: 1000,
-          smooth: 'easeInOutQuart',
+          smooth: "easeInOutQuart",
         });
       }, delay);
     }
   }, [isLoaded]);
   const LoadingPage = () => {
     return (
-      <div className='loading-page'>
-        <Container className='img-container'>
-          <Image src={img1} className="img-fluid image-loading" style={{ maxWidth: "50%"}} />
+      <div className="loading-page">
+        <Container className="img-container">
+          <Image
+            src={img1}
+            className="img-fluid image-loading"
+            style={{ maxWidth: "50%" }}
+          />
         </Container>
       </div>
     );
@@ -51,50 +57,56 @@ function App() {
     }
   }, [isLoaded]);
   return (
-    <div>
-      {isLoaded ? (
-        <div className={`App ${isLoading ? "" : "hidden"}`}>
-          <Navi />
-          <Link
-            to="Comp"
-            smooth={true}
-            duration={500}
-            delay={0}
-            offset={-50}
-            spy={true}
-            exact="true"
-            className="scroll-link"
-          ></Link>
-          <div className="sec1">
-            <div id="Home"></div>
-            <Carou />
+    <Router>
+      <div>
+        {isLoaded ? (
+          <div className={`App ${isLoading ? "" : "hidden"}`}>
+            <Navi />
+            <Link
+              to="Comp"
+              smooth={true}
+              duration={500}
+              delay={0}
+              offset={-50}
+              spy={true}
+              exact="true"
+              className="scroll-link"
+            ></Link>
+            <div className="sec1">
+              <div id="Home"></div>
+              <Carou />
+            </div>
+            <div className="sec2">
+              <div id="About"></div>
+              <About />
+            </div>
+            <div className="sec3" ref={targetRef}>
+              <div id="Comp"></div>
+              <Blog />
+            </div>
+            <div className="sec4">
+              <Sponsor />
+            </div>
+            <div className="sec2">
+              <div id="Talkshow"></div>
+              <Talkshow />
+            </div>
+            <div className="sec2">
+              <div id="FaQ"></div>
+              <FaQ />
+            </div>
+            <Foot />
           </div>
-          <div className="sec5">
-            <div id="About"></div>
-            <About />
-          </div>
-          <div className="sec2" ref={targetRef}>
-            <div id="Comp"></div>
-            <Blog />
-          </div>
-          <div className="sec4">
-            <Sponsor />
-          </div>
-          <div className="sec2">
-            <div id="Talkshow"></div>
-            <Talkshow />
-          </div>
-          <div className="sec3">
-            <div id="FaQ"></div>
-            <FaQ />
-          </div>
-          <Foot />
-        </div>
-      ) : (
-        <LoadingPage />
-      )}
-    </div>
+        ) : (
+          <LoadingPage />
+        )}
+      </div>
+      <Routes>
+        <Route path="/competition" element={<Competition />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
