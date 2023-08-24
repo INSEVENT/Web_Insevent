84dfd132fbacf0cc2cf4bba3e6d0811b72c08545
noviantisafitri <121856489+noviantisafitri@users.noreply.github.com>
2023-08-24 08:54:30 +0800

    Update card.js

--- src/data/card.js
+++ src/data/card.js
@@ -8,25 +8,24 @@ function getCurrentTime() {
 const cardData = [
   {
     imgSrc: img1,
-    category: "Category",
     title: "Competition",
     time: getCurrentTime(),
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, 
-    dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, 
-    varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, 
-    non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. 
-    Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue.`,
+    description: `INSEVENT mempersembahkan berbagai lomba kreatif yang menarik. Dalam kesempatan ini, 
+    INSEVENT akan menyajikan rangkaian kompetisi yang beragam, menantang, dan memberikan kesempatan bagi peserta untuk mengekspresikan bakat serta kreativitasnya 
+    dalam berbagai bidang.
+    Selain memberikan kesempatan bagi para peserta untuk mengekspresikan bakat dan kreativitasnya, 
+    INSEVENT juga bertekad untuk menghadirkan pengalaman kompetisi yang mengesankan. `,
     readMore: "/competition",
-    paragraf1: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, 
-    dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, 
-    varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, 
-    non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. 
-    Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue.`,
+    comp1: `Poster`,
+    comp2: `UI/UX`,
+    comp3: `E-Sport (Mobile Legend)`,
+    comp4: `Dance`,
+    comp5: `Band`,
+    comp6: `Video Kreatif`,
   },
 
   {
     imgSrc: img1,
-    category: "Category",
     title: "Talkshow",
     time: getCurrentTime(),
     description:  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, 
@@ -44,7 +43,6 @@ const cardData = [
 
   {
     imgSrc: img1,
-    category: "Category",
     title: "Pameran",
     time: getCurrentTime(),
     description:  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, 
@@ -62,7 +60,6 @@ const cardData = [
 
   {
     imgSrc: img1,
-    category: "Category",
     title: "Bazar",
     time: getCurrentTime(),
     description:  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, 
@@ -77,6 +74,23 @@ const cardData = [
     non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. 
     Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue.`,
   },
+  {
+    imgSrc: img1,
+    title: "Charity",
+    time: getCurrentTime(),
+    description:  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, 
+    dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, 
+    varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, 
+    non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. 
+    Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue.`,
+    readMore: "/bazar",
+    paragraf1: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, 
+    dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, 
+    varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, 
+    non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. 
+    Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue.`,
+  },
+  
 ];
 
 export default cardData;
