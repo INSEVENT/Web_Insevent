917deac0250ea6ce95b889da1330404bc8cfa343
zaki fauzan <zakifauzanrabb@gmail.com>
2023-09-22 01:51:29 +0800

    add some design

--- src/section/Sponsor.js
+++ src/section/Sponsor.js
@@ -1,6 +1,13 @@
 import { Container } from "react-bootstrap";
 import Inforsa from "../assets/img/inforsa.png";
 import Insevent from "../assets/img/logo.png";
+import badak from "../assets/img/logo/Badak.png";
+import hmti from "../assets/img/logo/HMTI.png";
+import permikomnas from "../assets/img/logo/permikomnas-removebg-preview.png";
+import psi from "../assets/img/logo/bempsi.png";
+import wgm from "../assets/img/logo/widyagama.png";
+import smdpos from "../assets/img/logo/samarinda pos.jpg";
+import bemkm from "../assets/img/logo/Logo BEM KM.png";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
@@ -31,7 +38,7 @@ const Sponsor = () => {
           className="sponsor-swiper"
         >
           <SwiperSlide>
-            <img src={Inforsa} alt="Inforsa Logo" className="img-sponsor-medpart"/>
+            <img src={bemkm} alt="Inforsa Logo" className="img-sponsor-medpart"/>
           </SwiperSlide>
           <SwiperSlide>
             <img src={Insevent} alt="Insevent Logo" className="img-sponsor-medpart"/>
@@ -40,10 +47,22 @@ const Sponsor = () => {
             <img src={Inforsa} alt="Inforsa Logo" className="img-sponsor-medpart"/>
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Insevent} alt="Insevent Logo" className="img-sponsor-medpart"/>
+            <img src={hmti} alt="Insevent Logo" className="img-sponsor-medpart"/>
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Inforsa} alt="Inforsa Logo" className="img-sponsor-medpart"/>
+            <img src={badak} alt="Inforsa Logo" className="img-sponsor-medpart"/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={permikomnas} alt="Inforsa Logo" className="img-sponsor-medpart"/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={psi} alt="Inforsa Logo" className="img-sponsor-medpart"/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={wgm} alt="Inforsa Logo" className="img-sponsor-medpart"/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={smdpos} alt="Inforsa Logo" className="img-sponsor-medpart"/>
           </SwiperSlide>
         </Swiper>
       </div>
